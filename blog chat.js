a546a6adabdede77f242cbe0afb0cbee1ea23fb0
SafeZone18 <44813998+SafeZone18@users.noreply.github.com>
2019-09-17 10:15:51 -0500

    Update blog chat.js

--- blog chat.js
+++ blog chat.js
@@ -1,2 +1,24 @@
 <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script src="js/app.js"></script>
+
+$(function() {
+  $('#commentform').submit(handleSubmit);
+});
+
+function handleSubmit() {
+  var form = $(this);
+  var data = {
+    "comment_author": form.find('#comment_author').val(),
+    "email": form.find('#email').val(),
+    "comment": form.find('#comment').val(),
+    "comment_post_ID": form.find('#comment_post_ID').val()
+  };
+
+  postComment(data);
+
+  return false;
+}
+
+function postComment(data) {
+  // send the data to the server
+}
