12b350162507bf3a9292c4f6e409f89628acefd1
SafeZone18 <44813998+SafeZone18@users.noreply.github.com>
2019-10-09 10:38:40 -0500

    Update blog chat.js

--- blog chat.js
+++ blog chat.js
@@ -68,5 +68,40 @@ function parseDisplayDate(date) {
   return display;
 }
 
+$ajax = ($_SERVER[ 'HTTP_X_REQUESTED_WITH' ] === 'XMLHttpRequest');
 
+<?php
+require('Persistence.php');
 
+$ajax = ($_SERVER[ 'HTTP_X_REQUESTED_WITH' ] === 'XMLHttpRequest');
+
+$db = new Persistence();
+$added = $db->add_comment($_POST);
+
+if($ajax) {
+  sendAjaxResponse($added);
+}
+else {
+  sendStandardResponse($added); 
+}
+
+function sendAjaxResponse($added) {
+  header("Content-Type: application/x-javascript");
+  if($added) {
+    header( 'Status: 201' );
+    echo( json_encode($added) );
+  }
+  else {
+    header( 'Status: 400' );
+  }
+}
+
+function sendStandardResponse($added) {
+  if($added) {
+    header( 'Location: index.php' );
+  }
+  else {
+    header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
+  }
+}
+?>
